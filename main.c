5e79e53a929cee1df5f724f0747dfa1fe3ec9bca
Ic3Venom <Ic3Venom@gmail.com>
2017-01-02 22:08:45 -0800

    Added shift keys, problem with '\"'
    '\"' counts the '\' in the index when comparing layouts, problem might be fixed tomorrow.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TOTAL_KEYS  46
+#define TOTAL_KEYS  95
 #define LAYOUTS     4
 #define MAX_LENGTH  50
 
@@ -9,16 +9,16 @@
 
 void layoutSelect(int layoutOrder[]);
 void conversion  (int layoutOrder[]);
-void dvorak      (char layout[][46], int i);
-void qwerty      (char layout[][46], int i);
-void colemak     (char layout[][46], int i);
-void workman     (char layout[][46], int i);
+void dvorak      (char layout[][TOTAL_KEYS], int i);
+void qwerty      (char layout[][TOTAL_KEYS], int i);
+void colemak     (char layout[][TOTAL_KEYS], int i);
+void workman     (char layout[][TOTAL_KEYS], int i);
 
 int main()
 {
     int layoutOrder[2];
-    layoutSelect(layoutOrder);
 
+    layoutSelect(layoutOrder);
 
     conversion(layoutOrder);
 
@@ -29,9 +29,9 @@ void layoutSelect(int layoutOrder[])
 {
     char userInput[MAX_LENGTH];
     char layoutSupport[LAYOUTS][8] = {"dvorak","qwerty","colemak","workman"};
-    int  i;
+    int  i, j;
 
-    for (i= 0; i < 2;)
+    for (i = 0; i < 2;)
     {
         if (i == 1)
         {
@@ -45,22 +45,22 @@ void layoutSelect(int layoutOrder[])
 
         if(strcmp(userInput, layoutSupport[0]) == 0)
         {
-            layoutOrder[i]= 0;
+            layoutOrder[i] = 0;
             i++;
         }
         else if (strcmp(userInput, layoutSupport[1]) == 0)
         {
-            layoutOrder[i]= 1;
+            layoutOrder[i] = 1;
             i++;
         }
         else if (strcmp(userInput, layoutSupport[2]) == 0)
         {
-            layoutOrder[i]= 2;
+            layoutOrder[i] = 2;
             i++;
         }
         else if (strcmp(userInput, layoutSupport[3]) == 0)
         {
-            layoutOrder[i]= 3;
+            layoutOrder[i] = 3;
             i++;
         }
         else if (strcmp(userInput, "help") == 0)
@@ -72,10 +72,9 @@ void layoutSelect(int layoutOrder[])
         }
         else if (strcmp(userInput, "list") == 0)
         {
-            int j;
-
             printf("List of supported layouts:\n");
-            for(j= 0; j < LAYOUTS; j++)
+
+            for(j = 0; j < LAYOUTS; j++)
             {
                 printf("%d: %s\n", j +1, layoutSupport[j]);
             }
@@ -112,29 +111,26 @@ void conversion(int layoutOrder[])
     char    layout[2][TOTAL_KEYS];
     char    userInput[MAX_LENGTH];
     char    translatedOutput[MAX_LENGTH];
+    int     i, j;
 
     //Puts each layout into [layout]
+    for(i = 0; i < 2; i++)
     {
-        int i;
-
-        for(i= 0; i < 2; i++)
+        printf("%d", i);
+        switch(layoutOrder[i])
         {
-            printf("%d", i);
-            switch(layoutOrder[i])
-            {
-            case 0:
-                dvorak(layout, i);
-                break;
-            case 1:
-                qwerty(layout, i);
-                break;
-            case 2:
-                colemak(layout, i);
-                break;
-            case 3:
-                workman(layout, i);
-                break;
-            }
+        case 0:
+            dvorak(layout, i);
+            break;
+        case 1:
+            qwerty(layout, i);
+            break;
+        case 2:
+            colemak(layout, i);
+            break;
+        case 3:
+            workman(layout, i);
+            break;
         }
     }
 
@@ -143,47 +139,41 @@ void conversion(int layoutOrder[])
     getchar();
     fgets (userInput, MAX_LENGTH, stdin);
 
-            printf("\nDEBUG:\n*userInput: %s|", userInput);
-            printf("\n*layout[0]: %s", layout[0]);
-            printf("\n*layout[1]: %s\n", layout[1]);
-
-    //In theory this is easy, but I'm still not the greatest with simple things
+    for(i = 0; i < MAX_LENGTH; i++)
     {
-        int i, j;
 
-        for(i= 0; i < MAX_LENGTH; i++)
+        for(j = 0; j < TOTAL_KEYS; j++)
         {
-            for(j= 0; j < TOTAL_KEYS; j++)
-            {
-                if (userInput[i] == layout[0][j])
-                {
-                    printf("pop\n");
-                    translatedOutput[i] = layout[1][j];
-                    break;
-                }
 
+            if (userInput[i] == layout[0][j])
+            {
+                printf("%c", layout[1][j]);
+                break;
             }
         }
     }
-    printf("%s", translatedOutput);
 }
 
 void dvorak (char layout[][TOTAL_KEYS], int i)
 {
-    strncpy(layout[i], "1234567890[]',.pyfgcrl/=aoeuidhtns-;qjkxbmwvz", TOTAL_KEYS);
+    //!@#$%^&*(){}\"<>PYFGCRL?+\AOEUIDHTNS_:QJKXBMWVZ
+    strncpy(layout[i], " 1234567890[]',.pyfgcrl/=\aoeuidhtns-;qjkxbmwvz", TOTAL_KEYS);
 }
 
 void qwerty (char layout[][TOTAL_KEYS], int i)
 {
-    strncpy(layout[i], "1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./", TOTAL_KEYS);
+    //!@#$%^&*()_+QWERTYUIOP{}\ASDFGHJKL:\"ZXCVBNM<>?
+    strncpy(layout[i], " 1234567890-=qwertyuiop[]\asdfghjkl;'zxcvbnm,./", TOTAL_KEYS);
 }
 
 void colemak(char layout[][TOTAL_KEYS], int i)
 {
-    strncpy(layout[i], "1234567890-=qwfpgjluy;{}arstdhneio'zxcvbkm,./", TOTAL_KEYS);
+    //!@#$%^&*()_+QWFPGJLUY:{}\ARSTDHNEIO\"ZXCVBXM<>?
+    strncpy(layout[i], " 1234567890-=qwfpgjluy;[]\arstdhneio'zxcvbkm,./", TOTAL_KEYS);
 }
 
 void workman(char layout[][TOTAL_KEYS], int i)
 {
-    strncpy(layout[i], "1234567890-=qdrwbjfup;{}ashtgyneoi'zxmcvkl,./", TOTAL_KEYS);
+    //!@#$%^&*()_+QDRWBJFUP:{}\ASHTGYNEOI\"ZXMCVKL<>?
+    strncpy(layout[i], " 1234567890-=qdrwbjfup;[]\ashtgyneoi'zxmcvkl,./", TOTAL_KEYS);
 }
